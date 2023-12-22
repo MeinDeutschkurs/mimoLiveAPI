1f4be7e71b0d03b1f8303dc7228db1af4aa1faef
MeinDeutschkurs <129950466+MeinDeutschkurs@users.noreply.github.com>
2023-12-22 05:57:27 +0100

    port is now configureable

--- script/server.py
+++ script/server.py
@@ -1,10 +1,29 @@
 from flask import Flask, jsonify
 import subprocess
+import configparser
+
+# Function to load configuration from an INI file
+def load_config(filename):
+    config = configparser.ConfigParser()
+    try:
+        config.read(filename)
+        return config
+    except FileNotFoundError:
+        print(f"Config file '{filename}' not found.")
+        exit(1)
+
+# Load configuration from the INI file
+config = load_config('../config.ini')
+
+if config:
+    # Get BASE_URL from the configuration
+    SERVER_PORT = float(config.get('SCRIPT', 'SERVER_PORT', fallback='8888'))
 
 app = Flask(__name__)
 
 @app.route('/<path:script>', strict_slashes=False)
 def run_python_script(script):
+    global SERVER_PORT
     # create path
     script_path = f"./modules/{script}.py"
     try:
@@ -16,4 +35,4 @@ def run_python_script(script):
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8888)
+    app.run(host='localhost', port=SERVER_PORT)
